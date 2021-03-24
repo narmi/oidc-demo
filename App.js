5e8fcc4c3de03a9de6f46fdabc1312a05b745ee5
zrbecker <zachary@narmi.com>
2021-03-24 13:35:00 -0400

    Added OIDC to create-react-native-app skeleton

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { authorize } from "react-native-app-auth";
+
+const OIDC_CONFIG = {
+  issuer: "https://accounts.google.com",
+  clientId:
+    "451923530993-rquf590ndjclm4vih3bf27rq59dfm7gj.apps.googleusercontent.com",
+  redirectUrl:
+    "com.googleusercontent.apps.451923530993-rquf590ndjclm4vih3bf27rq59dfm7gj:/oauth2redirect/google",
+  scopes: ["openid", "profile"],
+};
 
 export default function App() {
+  const [token, setToken] = useState(null);
+
+  const loginWithOIDC = async () => {
+    try {
+      const result = await authorize(OIDC_CONFIG);
+      setToken(result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const logout = () => {
+    setToken(null);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {token === null ? (
+        <Text onPress={loginWithOIDC}>Login with OIDC</Text>
+      ) : (
+        <Text onPress={logout}>Logout</Text>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +44,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
